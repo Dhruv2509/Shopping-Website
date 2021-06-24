512f5cbb707b8888f6b62a90c639d2ddc0246bc3
Dhruv2509 <47301407+Dhruv2509@users.noreply.github.com>
2021-06-24 19:11:02 +0530

    Add files via upload

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,10 @@
 import express from 'express'
-import { getProductById, getProducts } from '../controllers/productController.js'
-
+import { getProducts, getProductsById } from '../controllers/productControllers.js'
 const router = express.Router()
 
 router.route('/').get(getProducts)
+router.route('/:id').get(getProductsById)
 
-router.route('/:id').get(getProductById)
 
 
 
