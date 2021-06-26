9cecac634972e817c94b7c76b40a1ac3de61a59d
Dhruv2509 <47301407+Dhruv2509@users.noreply.github.com>
2021-06-26 19:35:42 +0530

    Add files via upload

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -12,58 +12,99 @@ const addOrderItems = asyncHandler(async (req, res) => {
     totalPrice,
   } = req.body;
 
-  if(orderItems && orderItems.length == 0) {
-    res.status(400)
-    throw new Error('No order items')
+  if (orderItems && orderItems.length == 0) {
+    res.status(400);
+    throw new Error("No order items");
   } else {
-      const order = new Order({
-        orderItems,
-        user: req.user._id,
-        shippingAddress,
-        paymentMethod,
-        itemsPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice,
-      })
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
 
-      const createdOrder = await order.save()
+    const createdOrder = await order.save();
 
-      res.status(201).json(createdOrder)
+    res.status(201).json(createdOrder);
   }
 });
 
 const getOrderById = asyncHandler(async (req, res) => {
-      const order = await Order.findById(req.params.id).populate('user', 'name email')
-      if(order) {
-        res.json(order)
-      } else {
-        res.status(404)
-        throw new Error('Order not found')
-      }
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
   }
-);
+});
 
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  const order = await Order.findById(req.params.id)
-  if(order) {
-    order.isPaid = true
-    order.paidAt = Date.now()
+  const order = await Order.findById(req.params.id);
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
     order.paymentResult = {
       id: req.body.id,
       status: req.body.status,
       update_time: req.body.update_time,
       email_address: req.body.payer.email_address,
-    }
+    };
 
-    const updatedOrder = await order.save()
+    const updatedOrder = await order.save();
 
-    res.json(updatedOrder)
+    res.json(updatedOrder);
   } else {
-    res.status(404)
-    throw new Error('Order not found')
+    res.status(404);
+    throw new Error("Order not found");
   }
-}
-);
+});
 
-export { addOrderItems, getOrderById, updateOrderToPaid }
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
+});
+
+//@desc get all orders
+//@route GET /api/orders
+//@access Private/Admin
+
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate("user", "id name");
+  res.json(orders);
+});
+
+//@desc update order to delivered
+//@route GET /api/orders/:id/deliver
+//@access Private/Admin
+
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
+  getOrders,
+  updateOrderToDelivered,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express'
-import { getProducts, getProductsById } from '../controllers/productControllers.js'
+import { createProduct, createProductReview, getTopProducts, updateProduct } from '../controllers/productControllers.js'
+import { deleteProduct, getProducts, getProductsById } from '../controllers/productControllers.js'
+import { protect,admin } from '../middleware/authMiddleware.js'
+
 const router = express.Router()
 
-router.route('/').get(getProducts)
-router.route('/:id').get(getProductsById)
+router.route('/').get(getProducts).post(protect,admin,createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts);
+router.route('/:id').get(getProductsById).delete(protect,admin,deleteProduct).put(protect,admin,updateProduct)
 
 
 
